820c869755ce8bf7511a8e3f9fa57260398b7ff5
Thisisyusub <kanan.yusub@gmail.com>
2022-03-24 23:07:29 +0400

    source selection button completed

--- lib/presentation/pages/source_selection/source_selection_page.dart
+++ lib/presentation/pages/source_selection/source_selection_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:theme/theme.dart';
 
+import '../../../core/utils/selection.dart';
+import '../../../domain/entities/news_source.dart';
 import '../../bloc/news_source/news_source_cubit.dart';
 import '../main/main_page.dart';
 
@@ -34,71 +36,114 @@ class SourceSelectionPage extends StatelessWidget {
                   textAlign: TextAlign.center,
                 ),
                 const SizedBox(height: 30),
-                Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemBuilder: (_, i) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              flex: 2,
-                              child: Text(
-                                newsSources[i].value.title,
-                                style: appTheme.typography.title.copyWith(
-                                  color: appTheme.colors.secondaryColor,
-                                ),
-                              ),
-                            ),
-                            CupertinoSwitch(
-                              value: newsSources[i].selected,
-                              activeColor: appTheme.colors.secondaryColor,
-                              onChanged: (_) {
-                                sourceCubit.changeSelection(i);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    itemCount: newsSources.length,
-                  ),
+                _NewsSources(
+                  newsSources: newsSources,
+                  appTheme: appTheme,
+                  sourceCubit: sourceCubit,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      sourceCubit.submit();
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) {
-                            return const MainPage();
-                          },
-                        ),
-                      );
-                    },
-                    child: Text(localization.goForward),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        appTheme.colors.secondaryColor,
-                      ),
-                      textStyle: MaterialStateProperty.all(
-                        appTheme.typography.title.copyWith(
-                          color: appTheme.colors.secondaryColor,
-                        ),
-                      ),
-                      fixedSize: MaterialStateProperty.all(
-                        const Size(200.0, 50.0),
-                      ),
-                    ),
-                  ),
+                _SubmitButton(
+                  sourceCubit: sourceCubit,
+                  appTheme: appTheme,
                 ),
               ],
             );
           },
         ),
+      ),
+    );
+  }
+}
+
+class _SubmitButton extends StatelessWidget {
+  const _SubmitButton({
+    Key? key,
+    required this.sourceCubit,
+    required this.appTheme,
+  }) : super(key: key);
+
+  final NewsSourceCubit sourceCubit;
+  final AppThemeData appTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    final localization = AppLocalizations.of(context)!;
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ElevatedButton(
+        onPressed: () {
+          sourceCubit.submit();
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (_) {
+                return const MainPage();
+              },
+            ),
+          );
+        },
+        child: Text(localization.goForward),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            appTheme.colors.secondaryColor,
+          ),
+          textStyle: MaterialStateProperty.all(
+            appTheme.typography.title.copyWith(
+              color: appTheme.colors.secondaryColor,
+            ),
+          ),
+          fixedSize: MaterialStateProperty.all(
+            const Size(200.0, 50.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NewsSources extends StatelessWidget {
+  const _NewsSources({
+    Key? key,
+    required this.newsSources,
+    required this.appTheme,
+    required this.sourceCubit,
+  }) : super(key: key);
+
+  final List<Selection<NewsSource>> newsSources;
+  final AppThemeData appTheme;
+  final NewsSourceCubit sourceCubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemBuilder: (_, i) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    newsSources[i].value.title,
+                    style: appTheme.typography.title.copyWith(
+                      color: appTheme.colors.secondaryColor,
+                    ),
+                  ),
+                ),
+                CupertinoSwitch(
+                  value: newsSources[i].selected,
+                  activeColor: appTheme.colors.secondaryColor,
+                  onChanged: (_) {
+                    sourceCubit.changeSelection(i);
+                  },
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: newsSources.length,
       ),
     );
   }
